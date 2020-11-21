7ac2c00dda6c943d3b2d0904cdf81faad6ad2b8d
ThorstenKnepper <70054684+ThorstenKnepper@users.noreply.github.com>
2020-11-21 23:17:42 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,87 +1,39 @@
-"""
-
-Wie wird Bewegung registriert?
-
-Alle Richtungen haben einen maximal, minimal und Durchschnittswert über eine Zeitdauer.
-
-Wenn die Pegel (von min bis max) innerhalb der Zeitdauer einen Grenzwert überschreiten und die Anzahl dieser Überschreitungen einen Grenzwert "Häufigkeit" überschreitet: Dann liegt Bewegung vor
-
-Zeitraum: 20 Sekunden
-
-Häufigkeit: 5 Mal
-
-Grenzwert: 100
-
-"""
-"""
-
-Programmablauf:
-
-    Zu Beginn: Warten 100ms? Danach Bewegungsmesser starten
-
-Zeit starten, in der Bewegungsregistration stattfindet.
-
-Wenn keine Bewegung erkannt wurde (innerhalb der Zeit), dann Ausgabe
-
-Reset der Ausgabe, wenn wieder Bewegung registriert wurde oder manueller Reset
-
-"""
-"""
-
-Bewegung registrieren
-
-Wenn Bewegung, dann Variable BEWEGUNG setzen: TRUE
-
-Wenn keine Bewegung, dann Variable Bewegung: FALSCH
-
-Grenzwert definieren: Wenn Grenzwert überschritten, dann Bewegung erkannt.
-
-Lage erfassen: Wenn horizontale Position, dann nicht aktiv.
-
-Dauer der Bewegung festlegen, bis es als Bewegung angesehen wird
-
-"""
-"""
-
-Fragen an Messungen:
-
-    Wie sieht die Bewegung aus, wenn man steht? --> Messwertaufnahme und Auswertung
-
-"""
 def Zeitmessung(Dauer: number):
-    global ZeitUnbewegt, boAlarm, boReset
-    boBewegungRegistriert = 0
-    basic.pause(1000)
-    ZeitUnbewegt += 1
-    if 60 * Dauer <= ZeitUnbewegt:
-        boAlarm = 1
-    if boReset > 0 or boBewegungRegistriert > 0:
-        boAlarm = 0
-        ZeitUnbewegt = 0
-        boReset = 0
-def AlarmAusgeben(Alarm: bool, Akustisch: bool, Optisch: bool):
-    if Alarm:
-        if Akustisch:
-            music.play_melody("C E C E C E C C ", 120)
-        if Optisch:
-            basic.set_led_color(0xff0000)
-            basic.pause(200)
-            basic.set_led_color(0x0000ff)
-            basic.pause(200)
-    else:
-        basic.set_led_color(0x000000)
+    global ZeitUnbewegt, boAlarm, boReset, boBewegungRegistriert
+    if boStarteMessung == 1:
+        basic.pause(1000)
+        ZeitUnbewegt += 1
+        if 60 * Dauer <= ZeitUnbewegt:
+            boAlarm = 1
+        if boReset > 0 or boBewegungRegistriert > 0:
+            boAlarm = 0
+            ZeitUnbewegt = 0
+            boReset = 0
+            boBewegungRegistriert = 0
 def BewegungErkennen():
-    pass
+    global boBewegungRegistriert, ZählerBewegungX, Last_x
+    # Neuer Block (ArrayListe) wird ausgewertet
+    if zeiger_Arrays == 1:
+        basic.set_led_color(0xff00ff)
+        boBewegungRegistriert = 0
+        ZählerBewegungX = 0
+    # Wurde eine Bewegung festgestellt?
+    if abs(Mittelwert_X - Last_x) > SchwellwertBewegung:
+        ZählerBewegungX += 1
+        serial.write_value("Zähler+", ZählerBewegungX)
+    if ZählerBewegungX > HäufigkeitBewegungsErkannt:
+        boBewegungRegistriert = 1
+        basic.set_led_color(0x00ff00)
+    Last_x = Mittelwert_X
 # Fehlerquellen:
-    #
+# 
 # Erschütterungen (Zugfahrt)
 # 
 # Calliope fällt runter
 # 
 # Calliope wird zur Seite gelegt und nicht bewegt.
 def MesseBeschleunigungXYZSt():
-    global zeiger_Arrays, Mittelwert_X, Mittelwert_Y, Mittelwert_Z, Mittelwert_Stärke, boArrayVoll, boToggle
-    zeiger_Arrays = 0
+    global Mittelwert_X, Mittelwert_Y, Mittelwert_Z, Mittelwert_Stärke, zeiger_Arrays, boArrayVoll, boToggle
     for index in range(20):
         Mittelwert_X = (input.acceleration(Dimension.X) + 19 * Mittelwert_X) / 20
         Mittelwert_Y = (input.acceleration(Dimension.Y) + 19 * Mittelwert_Y) / 20
@@ -106,8 +58,20 @@ def MesseBeschleunigungXYZSt():
         WertelisteY[zeiger_Arrays] = Mittelwert_Y
         WertelisteZ[zeiger_Arrays] = Mittelwert_Z
         WertelisteStärke[zeiger_Arrays] = Mittelwert_Stärke
-    led.toggle(0, 0)
     boToggle = 1 - boToggle
+    serial.write_value("x", Mittelwert_X)
+    serial.write_value("Zeigerarry", zeiger_Arrays)
+    BewegungErkennen()
+    return 0
+# Programmablauf:
+# 
+# Zu Beginn: Warten 100ms? Danach Bewegungsmesser starten
+# 
+# Zeit starten, in der Bewegungsregistration stattfindet.
+# 
+# Wenn keine Bewegung erkannt wurde (innerhalb der Zeit), dann Ausgabe
+# 
+# Reset der Ausgabe, wenn wieder Bewegung registriert wurde oder manueller Reset
 
 def on_button_pressed_a():
     global boStarteMessung, boReset
@@ -143,7 +107,6 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 Index = 0
 Zeiger_Senden = 0
 boTransfer = 0
-boStarteMessung = 0
 boToggle = 0
 WertelisteStärke: List[number] = []
 WertelisteZ: List[number] = []
@@ -152,20 +115,41 @@ WertelisteX: List[number] = []
 Mittelwert_Stärke = 0
 Mittelwert_Z = 0
 Mittelwert_Y = 0
+Last_x = 0
 Mittelwert_X = 0
-zeiger_Arrays = 0
+ZählerBewegungX = 0
+boBewegungRegistriert = 0
 boReset = 0
-boAlarm = 0
 ZeitUnbewegt = 0
+boStarteMessung = 0
+SchwellwertBewegung = 0
+HäufigkeitBewegungsErkannt = 0
+zeiger_Arrays = 0
 ArrayGröße = 0
 boArrayVoll = 0
+boAlarm = 0
+def AlarmAusgeben(Alarm: any, Akustisch: bool, Optisch: bool):
+    if Alarm:
+        if Akustisch:
+            music.play_melody("C E C E C E ", 120)
+    if Optisch:
+        basic.set_led_color(0xff0000)
+        basic.pause(200)
+        basic.set_led_color(0x0000ff)
+        basic.pause(200)
+    else:
+        basic.set_led_color(0x000000)
 boArrayVoll = 0
 serial.set_baud_rate(BaudRate.BAUD_RATE115200)
-ArrayGröße = 60
+ArrayGröße = 10
+zeiger_Arrays = 0
 # Dauer in Minuten
-ZeitMaxUnbeweglichkeit = 30
+ZeitMaxUnbeweglichkeit = 1
+# Dauer in Minuten
+HäufigkeitBewegungsErkannt = 5
+SchwellwertBewegung = 100
 # Fragen an Messungen:
-    #
+# 
 # Wie sieht die Bewegung aus, wenn man steht? --> Messwertaufnahme und Auswertung
 
 def on_forever():
@@ -183,4 +167,3 @@ def on_forever():
     else:
         basic.show_icon(IconNames.YES)
 basic.forever(on_forever)
-
--- main.ts
+++ main.ts
@@ -1,70 +1,19 @@
-/** 
-
-Wie wird Bewegung registriert?
-
-Alle Richtungen haben einen maximal, minimal und Durchschnittswert über eine Zeitdauer.
-
-Wenn die Pegel (von min bis max) innerhalb der Zeitdauer einen Grenzwert überschreiten und die Anzahl dieser Überschreitungen einen Grenzwert "Häufigkeit" überschreitet: Dann liegt Bewegung vor
-
-Zeitraum: 20 Sekunden
-
-Häufigkeit: 5 Mal
-
-Grenzwert: 100
-
-
- */
-/** 
-
-Programmablauf:
-
-    Zu Beginn: Warten 100ms? Danach Bewegungsmesser starten
-
-Zeit starten, in der Bewegungsregistration stattfindet.
-
-Wenn keine Bewegung erkannt wurde (innerhalb der Zeit), dann Ausgabe
-
-Reset der Ausgabe, wenn wieder Bewegung registriert wurde oder manueller Reset
-
-
- */
-/** 
-
-Bewegung registrieren
-
-Wenn Bewegung, dann Variable BEWEGUNG setzen: TRUE
-
-Wenn keine Bewegung, dann Variable Bewegung: FALSCH
-
-Grenzwert definieren: Wenn Grenzwert überschritten, dann Bewegung erkannt.
-
-Lage erfassen: Wenn horizontale Position, dann nicht aktiv.
-
-Dauer der Bewegung festlegen, bis es als Bewegung angesehen wird
-
-
- */
-/** 
-
-Fragen an Messungen:
-
-    Wie sieht die Bewegung aus, wenn man steht? --> Messwertaufnahme und Auswertung
-
-
- */
 function Zeitmessung(Dauer: number) {
     
-    let boBewegungRegistriert = 0
-    basic.pause(1000)
-    ZeitUnbewegt += 1
-    if (60 * Dauer <= ZeitUnbewegt) {
-        boAlarm = 1
-    }
-    
-    if (boReset > 0 || boBewegungRegistriert > 0) {
-        boAlarm = 0
-        ZeitUnbewegt = 0
-        boReset = 0
+    if (boStarteMessung == 1) {
+        basic.pause(1000)
+        ZeitUnbewegt += 1
+        if (60 * Dauer <= ZeitUnbewegt) {
+            boAlarm = 1
+        }
+        
+        if (boReset > 0 || boBewegungRegistriert > 0) {
+            boAlarm = 0
+            ZeitUnbewegt = 0
+            boReset = 0
+            boBewegungRegistriert = 0
+        }
+        
     }
     
 }
@@ -72,16 +21,16 @@ function Zeitmessung(Dauer: number) {
 function AlarmAusgeben(Alarm: any, Akustisch: boolean, Optisch: boolean) {
     if (Alarm) {
         if (Akustisch) {
-            music.playMelody("C E C E C E C C ", 120)
+            music.playMelody("C E C E C E ", 120)
         }
         
-        if (Optisch) {
-            basic.setLedColor(0xff0000)
-            basic.pause(200)
-            basic.setLedColor(0x0000ff)
-            basic.pause(200)
-        }
-        
+    }
+    
+    if (Optisch) {
+        basic.setLedColor(0xff0000)
+        basic.pause(200)
+        basic.setLedColor(0x0000ff)
+        basic.pause(200)
     } else {
         basic.setLedColor(0x000000)
     }
@@ -90,18 +39,36 @@ function AlarmAusgeben(Alarm: any, Akustisch: boolean, Optisch: boolean) {
 
 function BewegungErkennen() {
     
+    //  Neuer Block (ArrayListe) wird ausgewertet
+    if (zeiger_Arrays == 1) {
+        basic.setLedColor(0xff00ff)
+        boBewegungRegistriert = 0
+        ZählerBewegungX = 0
+    }
+    
+    //  Wurde eine Bewegung festgestellt?
+    if (Math.abs(Mittelwert_X - Last_x) > SchwellwertBewegung) {
+        ZählerBewegungX += 1
+        serial.writeValue("Zähler+", ZählerBewegungX)
+    }
+    
+    if (ZählerBewegungX > HäufigkeitBewegungsErkannt) {
+        boBewegungRegistriert = 1
+        basic.setLedColor(0x00ff00)
+    }
+    
+    Last_x = Mittelwert_X
 }
 
 //  Fehlerquellen:
-// 
+//  
 //  Erschütterungen (Zugfahrt)
 //  
 //  Calliope fällt runter
 //  
 //  Calliope wird zur Seite gelegt und nicht bewegt.
-function MesseBeschleunigungXYZSt() {
+function MesseBeschleunigungXYZSt(): number {
     
-    zeiger_Arrays = 0
     for (let index = 0; index < 20; index++) {
         Mittelwert_X = (input.acceleration(Dimension.X) + 19 * Mittelwert_X) / 20
         Mittelwert_Y = (input.acceleration(Dimension.Y) + 19 * Mittelwert_Y) / 20
@@ -133,10 +100,22 @@ function MesseBeschleunigungXYZSt() {
         WertelisteStärke[zeiger_Arrays] = Mittelwert_Stärke
     }
     
-    led.toggle(0, 0)
     boToggle = 1 - boToggle
+    serial.writeValue("x", Mittelwert_X)
+    serial.writeValue("Zeigerarry", zeiger_Arrays)
+    BewegungErkennen()
+    return 0
 }
 
+//  Programmablauf:
+//  
+//  Zu Beginn: Warten 100ms? Danach Bewegungsmesser starten
+//  
+//  Zeit starten, in der Bewegungsregistration stattfindet.
+//  
+//  Wenn keine Bewegung erkannt wurde (innerhalb der Zeit), dann Ausgabe
+//  
+//  Reset der Ausgabe, wenn wieder Bewegung registriert wurde oder manueller Reset
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     boStarteMessung = 1
@@ -172,7 +151,6 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
 let Index = 0
 let Zeiger_Senden = 0
 let boTransfer = 0
-let boStarteMessung = 0
 let boToggle = 0
 let WertelisteStärke : number[] = []
 let WertelisteZ : number[] = []
@@ -181,20 +159,31 @@ let WertelisteX : number[] = []
 let Mittelwert_Stärke = 0
 let Mittelwert_Z = 0
 let Mittelwert_Y = 0
+let Last_x = 0
 let Mittelwert_X = 0
-let zeiger_Arrays = 0
+let ZählerBewegungX = 0
+let boBewegungRegistriert = 0
 let boReset = 0
-let boAlarm = 0
 let ZeitUnbewegt = 0
+let boStarteMessung = 0
+let SchwellwertBewegung = 0
+let HäufigkeitBewegungsErkannt = 0
+let zeiger_Arrays = 0
 let ArrayGröße = 0
 let boArrayVoll = 0
+let boAlarm = 0
+
 boArrayVoll = 0
 serial.setBaudRate(BaudRate.BaudRate115200)
-ArrayGröße = 60
+ArrayGröße = 10
+zeiger_Arrays = 0
 //  Dauer in Minuten
-let ZeitMaxUnbeweglichkeit = 30
+let ZeitMaxUnbeweglichkeit = 1
+//  Dauer in Minuten
+HäufigkeitBewegungsErkannt = 5
+SchwellwertBewegung = 100
 //  Fragen an Messungen:
-// 
+//  
 //  Wie sieht die Bewegung aus, wenn man steht? --> Messwertaufnahme und Auswertung
 basic.forever(function on_forever() {
     //  Dauer in Minuten
